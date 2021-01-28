fdd9a4e6f984abb4afe28288db2b86f0a71a7d98
Owen <oc289@exeter.ac.uk>
2021-01-28 13:19:19 +0000

    Added a ski sharp Health style control

--- NavigationForms/ViewModels/CustomRenderViewModel.cs
+++ NavigationForms/ViewModels/CustomRenderViewModel.cs
@@ -6,8 +6,44 @@ namespace NavigationForms.ViewModels
 {
     public class CustomRenderViewModel : OnPropertyChangedImplementation, INavigatable
     {
+        public int RedValue
+        {
+            get { return redValue; }
+            set
+            {
+                redValue = value;
+                OnPropertyChanged();
+            }
+        }
+        private int redValue;
+
+        public int GreenValue
+        {
+            get { return greenValue; }
+            set
+            {
+                greenValue = value;
+                OnPropertyChanged();
+            }
+        }
+        private int greenValue;
+
+        public int BlueValue
+        {
+            get { return blueValue; }
+            set
+            {
+                blueValue = value;
+                OnPropertyChanged();
+            }
+        }
+        private int blueValue;
+
         public CustomRenderViewModel()
         {
+            RedValue = 20;
+            GreenValue = 40;
+            BlueValue = 120;
         }
 
         public void OnNavigateAway()
