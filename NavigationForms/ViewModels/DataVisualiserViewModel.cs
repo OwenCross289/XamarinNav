8f5daaf26e2abb1019cec59ed10c50335c69b34d
Owen Cross <59806069+OwenCross289@users.noreply.github.com>
2023-04-21 17:46:46 +0100

    Fix to latest Xamarin ready for MAUI port

--- NavigationForms/ViewModels/DataVisualiserViewModel.cs
+++ NavigationForms/ViewModels/DataVisualiserViewModel.cs
@@ -30,43 +30,6 @@ namespace NavigationForms.ViewModels
 
         public DataVisualiserViewModel()
         {
-            int pointCount = 5;
-            double[] ys1 = RandomDataSet(pointCount);
-            double[] ys2 = RandomDataSet(pointCount);
-
-            // create a series of bars and populate them with data
-            var seriesA = new OxyPlot.Series.ColumnSeries()
-            {
-                Title = "Series A",
-                StrokeColor = OxyColors.Black,
-                FillColor = OxyColors.Red,
-                StrokeThickness = 1
-            };
-
-            var seriesB = new OxyPlot.Series.ColumnSeries()
-            {
-                Title = "Series B",
-                StrokeColor = OxyColors.Black,
-                FillColor = OxyColors.Blue,
-                StrokeThickness = 1
-            };
-
-            for (int i = 0; i < pointCount; i++)
-            {
-                seriesA.Items.Add(new OxyPlot.Series.ColumnItem(ys1[i], i));
-                seriesB.Items.Add(new OxyPlot.Series.ColumnItem(ys2[i], i));
-            }
-
-            // create a model and add the bars into it
-            var plotModel = new PlotModel
-            {
-                Title = "Bar Graph (Column Series)"
-            };
-            plotModel.Axes.Add(new OxyPlot.Axes.CategoryAxis());
-            plotModel.Series.Add(seriesA);
-            plotModel.Series.Add(seriesB);
-
-            Model = plotModel;
 
         }
 
@@ -82,12 +45,7 @@ namespace NavigationForms.ViewModels
 
         private double[] RandomDataSet(int points = 5, double start = 100, double mult = 50)
         {
-            // return an array of difting random numbers
-            double[] values = new double[points];
-            values[0] = start;
-            for (int i = 1; i < points; i++)
-                values[i] = values[i - 1] + (rand.NextDouble() - .5) * mult;
-            return values;
+            return new[] { 0.0, 0.1 };
         }
 
         private Random rand = new Random(0);
